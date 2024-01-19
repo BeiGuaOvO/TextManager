1ad406f8636882177347fd3e5025d7cc4a575ec6
BeiGua <lovemc2021@163.com>
2024-01-19 22:35:19 +0800

    v1

---
+++ src/main/java/github/textmanager/TextManager.java
@@ -0,0 +1,17 @@
+package github.textmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TextManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
